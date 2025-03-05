6678c51d73c67a3c84fa723e468ade724b720a18
trducng <trungduc1992@gmail.com>
2025-03-05 15:05:01 +0700

    Base object interface

--- easyparser/base.py
+++ easyparser/base.py
@@ -1,27 +1,61 @@
-from dataclasses import dataclass, field
-from typing import Any, Optional
+import uuid
+from typing import Any
 
 
-@dataclass
 class Origin:
-    source: str
-    location: Any
-    getter: str
-    file_type: str
+    """Represent the origin of an object from another object
+
+    Args:
+        source_id: source object id.
+        location: location of the object in the source object.
+    """
+
+    def __init__(
+        self,
+        source_id: str,
+        location: None | dict = None,
+    ):
+        self.source_id = source_id
+        self.location = location
 
 
-@dataclass
-class Snippet:
-    id: str
-    text: str
-    content: Any
-    dtype: str
-    origin: Optional[Origin] = None
-    metadata: dict = field(default_factory=dict)
+class Obj:
+    """Mandatory fields for an object represented in `easyparser`.
 
-    # def save(self, path: str):
-    #     with open(path, "w") as f:
-    #         f.write(self.text)
+    Args:
+        id: unique identifier for the object.
+        mimetype: mimetype of the object, plays a crucial role in determining how an
+            object is processed and rendered.
+        content: content of the object, can be anything (text or bytes), that can be
+            understood from the mimetype.
+        text: text representation of the object.
+        parent: parent object id. Defaults to None.
+        children: list of children object ids. Defaults to None.
+        next: next object id. Defaults to None.
+        prev: previous object id. Defaults to None.
+        origin: the location of this object in relative to the parent.
+        metadata: metadata of the object, a free-style dictionary.
+    """
 
-    # export
-    # methods to compose snippets to a larger file
+    def __init__(
+        self,
+        mimetype: str,
+        content: Any = None,
+        text: str = "",
+        parent: None | str = None,
+        children: None | list = None,
+        next: None | str = None,
+        prev: None | str = None,
+        origin: None | Origin = None,
+        metadata: None | dict = None,
+    ):
+        self.id: str = uuid.uuid4().hex
+        self.mimetype = mimetype
+        self.content = content
+        self.text = text
+        self.parent = parent
+        self.children = children
+        self.next = next
+        self.prev = prev
+        self.origin = origin
+        self.metadata = metadata
