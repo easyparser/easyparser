5486d8b7395ea04f7229eb36406eb7cd2d54c9bc
trducng <trungduc1992@gmail.com>
2025-04-02 14:43:50 +0700

    test: test pandoc on docx

--- tests/test_parse/test_pandoc.py
+++ tests/test_parse/test_pandoc.py
@@ -4,15 +4,11 @@ from easyparser.base import Chunk
 from easyparser.mime import mime_docx
 from easyparser.parse.pandoc_engine import PandocEngine
 
-docx_path = str(Path(__file__).parent.parent / "assets" / "lz.md")
+docx_path = str(Path(__file__).parent.parent / "assets" / "with_image.docx")
 
 
-def test_pandoc():
+def test_pandoc_docx():
     root = mime_docx.as_root_chunk(docx_path)
     chunks = PandocEngine.run(root)
     chunk = list(chunks.iter_groups())[0][0]
     assert isinstance(chunk, Chunk)
-
-
-if __name__ == "__main__":
-    test_pandoc()
