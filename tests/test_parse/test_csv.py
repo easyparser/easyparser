8c4f79f654a3c66e626e42148e2dea93548cfabd
trducng <trungduc1992@gmail.com>
2025-04-23 15:29:38 +0700

    parse/csv: parse from csv file into Chunk representation

---
+++ tests/test_parse/test_csv.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from easyparser.base import Chunk
+from easyparser.mime import mime_csv
+from easyparser.parse.csv import CsvParser
+
+file_path = str(Path(__file__).parent.parent / "assets" / "contains_empty_cell.csv")
+
+
+def test_parse_csv():
+    root = mime_csv.as_root_chunk(file_path)
+    chunks = CsvParser.run(root)
+    chunk = chunks[0]
+    assert isinstance(chunk, Chunk)
