635ae4147d93ddc03528e5e9f54ead0304e81439
trducng <trungduc1992@gmail.com>
2025-05-08 18:33:47 +0700

    refactor: separate the mime operation from controller into a dedicted module

--- easyparser/controller.py
+++ easyparser/controller.py
@@ -1,90 +1,13 @@
 import hashlib
 import logging
-import mimetypes
 from contextlib import contextmanager
 from pathlib import Path
 from typing import Generator
 
-try:
-    from magika import Magika
-
-    _m = Magika()
-except ImportError:
-    _m = None
-
-try:
-    import magic
-except ImportError:
-    magic = None
-
 from easyparser.base import BaseOperation, Chunk, CType, Origin
-
-if hasattr(mimetypes, "guess_file_type"):
-    _mimetypes_guess_file = mimetypes.guess_file_type
-else:
-    _mimetypes_guess_file = mimetypes.guess_type
-
+from easyparser.mime import MimeType, guess_mimetype
 
 logger = logging.getLogger(__name__)
-
-
-def guess_mimetype(path, default: str = "application/octet-stream") -> str:
-    """Guess mimetype based on file path, prioritize magika > magic > mimetypes.
-
-    Args:
-        path: the path to the file
-        default: the mimetype to return if the mimetype cannot be guessed
-
-    Returns:
-        The mimetype of the file.
-    """
-    if Path(path).is_dir():
-        return MimeType.directory
-
-    if _m:
-        return _m.identify_path(path).output.mime_type
-    if magic:
-        return magic.from_file(path, mime=True)
-
-    guessed = _mimetypes_guess_file(path)[0]
-    if guessed:
-        return guessed
-
-    return default
-
-
-class MimeType:
-    # Text
-    text = "text/plain"
-    html = "text/html"
-    md = "text/markdown"
-
-    # Image
-    jpeg = "image/jpeg"
-    png = "image/png"
-
-    # Document
-    pdf = "application/pdf"
-    docx = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-    pptx = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-    xlsx = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-
-    # Data interchange
-    json = "application/json"
-    toml = "application/toml"
-    yaml = "application/yaml"
-    yaml_x = "application/x-yaml"
-
-    # Sound & video
-    wav = "audio/wav"
-    mp3 = "audio/mpeg"
-    mp4 = "audio/mp4"
-
-    # Archive
-    # zip = "application/zip"
-    # tar = "application/x-tar"
-    epub = "application/epub+zip"
-    directory = "inode/directory"
 
 
 class Controller:
