28e603a31f3fd2873add56775f27b379adeaa446
Tuan Anh Nguyen Dang <taprosoft@gmail.com>
2025-05-20 13:44:07 +0700

    feat: new demo app with PDF citation

--- gradio/app.py
+++ gradio/app.py
@@ -1,18 +1,23 @@
+import functools
 import os
 import time
 import zipfile
 from collections import defaultdict
 from pathlib import Path
 
-from gradio_pdf import PDF
+import pymupdf
 
 import gradio as gr
 from easyparser.base import CType
 from easyparser.controller import Controller
 from easyparser.parser import DoclingPDF, FastPDF, SycamorePDF, UnstructuredPDF
-from easyparser.parser.fastpdf.util import bytes_to_base64
+from easyparser.parser.fastpdf.util import OCRMode, bytes_to_base64
+from easyparser.split.toc_builder import TOCBuilder
 from easyparser.util.plot import plot_img, plot_pdf
 
+MAX_PAGES = os.getenv("MAX_PAGES", 10)
+MAX_PAGES_CHUNKING = os.getenv("MAX_PAGES", 50)
+MODEL_NAME = os.getenv("MODEL_NAME", "gemini-2.0-flash")
 METHOD_MAP = {
     "easyparser_fastpdf": FastPDF,
     "unstructured": UnstructuredPDF,
@@ -23,7 +28,89 @@ METHOD_LIST = list(METHOD_MAP.keys())
 TMP_DIR = Path("/tmp/visualize")
 TMP_DIR.mkdir(exist_ok=True)
 ctrl = Controller()
+
 DEBUG_DIR = Path("debug")
+PDF_JS_DIR = "pdfjs-4.0.379-dist"
+
+assert Path(PDF_JS_DIR).exists(), (
+    f"Missing `{PDF_JS_DIR}` directory. Please download it from"
+    "https://github.com/mozilla/pdf.js/releases"
+    "/download/v4.0.379/pdfjs-4.0.379-dist.zip"
+)
+
+HEAD_HTML = """
+<script type='module' src='https://cdn.skypack.dev/pdfjs-viewer-element'></script>"
+"""
+LOAD_PDF_JS = """
+function onPDFLoad(path) {
+    if (path == null) {
+        return;
+    }
+    path_str = "/gradio_api/file=" + path["path"];
+    pdf_js_viewer_element = document.getElementById("pdf-viewer");
+    pdf_js_viewer_element.setAttribute("src", path_str);
+    pdf_js_viewer_element.setAttribute("page", 1);
+    return path_str;
+}
+"""
+CLEAR_PDF_JS = """
+function clearPDF() {
+    var pdfViewer = document.getElementById("pdf-viewer");
+    pdfViewer.setAttribute("src", "");
+}
+"""
+ASSIGN_CLICK_JS = """
+function assignClickEvent() {
+    var elements = document.querySelectorAll("a.chunk-ref");
+    elements.forEach(function(element) {
+        element.addEventListener("click", function() {
+            var id = this.id;
+            var pdfViewer = document.getElementById("pdf-viewer");
+            var innerDoc = pdfViewer.iframe.contentDocument
+                ? pdfViewer.iframe.contentDocument
+                : pdfViewer.iframe.contentWindow.document;
+            var page = id.split("-")[0];
+            var bbox = id.split("-")[1];
+            var left, top, right, bottom;
+            if (bbox) {
+                // remove first and last char "[]" from bbox
+                bbox = bbox.substring(1, bbox.length - 1);
+                split_bbox = bbox.split(",");
+                left = parseFloat(split_bbox[0]) * 100;
+                top = parseFloat(split_bbox[1]) * 100;
+                right = parseFloat(split_bbox[2]) * 100;
+                bottom = parseFloat(split_bbox[3]) * 100;
+                width = right - left;
+                height = bottom - top;
+            }
+
+            pdfViewer.setAttribute("page", page);
+            var query_selector =
+                "#viewer > div[data-page-number='" +
+                page +
+                "'] > div.textLayer"
+            var textLayer = innerDoc.querySelector(query_selector);
+
+            // remove all previous annotation sections
+            if (!textLayer) {
+                return;
+            }
+            var previous_annotation_sections = textLayer.querySelectorAll("section.chunk-ref");
+            if (previous_annotation_sections.length > 0) {
+                previous_annotation_sections.forEach(function(section) {
+                    section.remove();
+                });
+            }
+
+            var new_annotation_section = document.createElement("section");
+            // set the position of the new section
+            new_annotation_section.setAttribute("class", "chunk-ref");
+            new_annotation_section.setAttribute("style", "position: absolute; z-index: 0; background-color: rgba(255, 255, 0, 1.0); left: " + left + "%; top: " + top + "%; width: " + width + "%; height: " + height + "%;");
+            textLayer.appendChild(new_annotation_section);
+        });
+    });
+}
+"""  # noqa: E501
 
 
 def format_chunk(chunk, include_image=True):
@@ -48,14 +135,47 @@ def format_chunk(chunk, include_image=True):
                 block_img_elem,
             )
     elif chunk.ctype == CType.Header:
-        block_text = f"### {chunk.text}"
+        block_text = f"### {chunk.content}"
     else:
-        block_text = chunk.text
+        block_text = chunk.content
     return block_text
 
 
+@functools.lru_cache(maxsize=None)
+def trim_pages(pdf_path, output_path=None, start_page=0, trim_pages=5):
+    doc = pymupdf.open(pdf_path)
+    if not output_path:
+        output_path = pdf_path[:-4] + "_trimmed.pdf"
+
+    num_pages = len(doc)
+    if trim_pages > 0 and num_pages > trim_pages:
+        to_select = list(range(start_page, min(start_page + trim_pages, num_pages)))
+        doc.select(to_select)
+        try:
+            doc.ez_save(output_path)
+        except:  # noqa: E722
+            for xref in range(1, doc.xref_length()):
+                try:
+                    _ = doc.xref_object(xref)
+                except:  # noqa: E722
+                    doc.update_object(xref, "<<>>")
+            doc.ez_save(output_path)
+
+        print("Trimmed pdf to path", output_path)
+    else:
+        return pdf_path
+
+    return output_path
+
+
 def convert_document(
-    pdf_path, method, use_full_page=False, force_ocr=False, enabled=True
+    pdf_path,
+    method,
+    use_full_page=False,
+    force_ocr=False,
+    generate_toc=False,
+    add_reference_links=True,
+    enabled=True,
 ):
     if enabled:
         print("Processing file", pdf_path, "with method", method)
@@ -65,18 +185,27 @@ def convert_document(
     if not pdf_path:
         raise ValueError("No file provided")
 
+    method = METHOD_MAP[method]
+    # trim to MAX_PAGES
+    if MAX_PAGES > 0 and MAX_PAGES_CHUNKING > 0:
+        max_pages = MAX_PAGES_CHUNKING if method == FastPDF else MAX_PAGES
+        pdf_path = trim_pages(
+            pdf_path,
+            start_page=0,
+            trim_pages=max_pages,
+        )
+
     # benchmarking
     start = time.time()
     debug_image_paths = []
 
     root = ctrl.as_root_chunk(pdf_path)
-    method = METHOD_MAP[method]
-
     if method == FastPDF:
         chunks = method.run(
             root,
+            # preset=ParserPreset.BEST,
             render_full_page=use_full_page,
-            ocr_mode="auto" if not force_ocr else "on",
+            ocr_mode=OCRMode.AUTO if not force_ocr else OCRMode.ON,
             use_layout_parser=True,
             render_2d_text_paragraph=True,
             extract_image=True,
@@ -86,24 +215,69 @@ def convert_document(
     else:
         chunks = method.run(root)
 
-    # serialie the child chunks to list
-    chunks = [chunk for _, chunk in chunks[0].walk() if chunk.ctype != CType.Root]
-    print("Total chunks:", len(chunks))
+    if generate_toc:
+        print("Generating Table-of-Content")
+        toc_text = (
+            "<details open='true'><summary><big>Table-of-Content</big></summary>\n\n"
+        )
+        new_chunks = TOCBuilder.run(
+            chunks,
+            use_llm=True,
+            model=MODEL_NAME,
+        )
+        for level, chunk in new_chunks[0].walk():
+            if chunk.ctype != CType.Header:
+                continue
 
-    max_page = 0
+            chunk_page = chunk.origin["location"]["page"]
+            chunk_bbox = chunk.origin["location"]["bbox"]
+            chunk_text = (
+                "<h4>" + "".join(["&nbsp;&nbsp;&nbsp;"] * level) + chunk.content
+            )
+            ref_link = (
+                f" <a class='chunk-ref' id='{chunk_page}-{chunk_bbox}'>[↗]</a>"
+                if add_reference_links
+                else ""
+            )
+            chunk_text += ref_link + "</h4>"
+            toc_text += chunk_text + "\n\n"
+
+        toc_text += "</details>\n\n---\n\n"
+    else:
+        toc_text = ""
+
+    # serialize the child chunks to list
+    child_chunks = [chunk for _, chunk in chunks[0].walk() if chunk.ctype != CType.Root]
+    print("Total chunks:", len(child_chunks))
+
+    max_page = 1
     print("Table Of Content")
-    for chunk in chunks:
+    for chunk in child_chunks:
         if chunk.ctype == CType.Header:
-            print(chunk.text)
+            print(chunk.content)
         max_page = max(max_page, chunk.origin.location["page"])
 
-    rendered_texts = [format_chunk(chunk) for chunk in chunks]
+    rendered_texts = []
+    for chunk in child_chunks:
+        chunk_page = chunk.origin.location["page"]
+        chunk_bbox = chunk.origin.location["bbox"]
+        ref_link = (
+            f"\n<a class='chunk-ref' id='{chunk_page}-{chunk_bbox}'>[↗]</a>"
+            if add_reference_links
+            else ""
+        )
+        text = format_chunk(chunk) + ref_link
+        rendered_texts.append(text)
+
     rendered_texts_no_img = [
-        format_chunk(chunk, include_image=False) for chunk in chunks
+        format_chunk(chunk, include_image=False) for chunk in child_chunks
     ]
     # remove empty strings
-    combined_text = "\n\n".join([text for text in rendered_texts if text])
+    combined_text = toc_text + "\n\n".join([text for text in rendered_texts if text])
     combined_text_no_img = "\n\n".join([text for text in rendered_texts_no_img if text])
+
+    # alternative rendering
+    # combined_text = combined_text_no_img = chunks[0].render("markdown")
 
     duration = time.time() - start
     duration_per_page = duration / max_page
@@ -118,9 +292,9 @@ def convert_document(
     debug_dir.mkdir(exist_ok=True)
 
     if pdf_path.endswith(".pdf"):
-        plot_pdf(pdf_path, chunks, debug_dir)
+        plot_pdf(pdf_path, child_chunks, debug_dir)
     else:
-        plot_img(pdf_path, chunks, debug_dir)
+        plot_img(pdf_path, child_chunks, debug_dir)
 
     debug_image_paths = list(debug_dir.glob("*.png"))
 
@@ -192,8 +366,9 @@ with gr.Blocks(
     theme=gr.themes.Ocean(
         font_mono="PT Mono",
     ),
+    head=HEAD_HTML,
 ) as demo:
-    with open("gradio/header.html") as file:
+    with open("header.html") as file:
         header = file.read()
     gr.HTML(header)
     output_components = []
@@ -201,7 +376,7 @@ with gr.Blocks(
     visualization_sub_tabs = []
 
     with gr.Row():
-        with gr.Column(variant="panel", scale=3):
+        with gr.Column(variant="panel", scale=4):
             input_file = gr.File(
                 label="Upload PDF document",
                 file_types=[
@@ -215,7 +390,7 @@ with gr.Blocks(
                 ],
             )
             with gr.Accordion("Examples:"):
-                example_root = "./"
+                example_root = "pdfs/"
                 gr.Examples(
                     examples=[
                         os.path.join(example_root, _)
@@ -248,16 +423,32 @@ with gr.Blocks(
                     value=False,
                 )
             with gr.Row():
+                generate_toc = gr.Checkbox(
+                    label="Generate Table-of-Content",
+                    value=False,
+                )
+                add_reference_links = gr.Checkbox(
+                    label="Add reference links to original PDF",
+                    value=True,
+                )
+            with gr.Row():
                 convert_btn = gr.Button("Convert", variant="primary", scale=2)
                 clear_btn = gr.ClearButton(value="Clear", scale=1)
 
     with gr.Row():
-        with gr.Column(variant="panel", scale=3):
-            pdf_preview = PDF(
-                label="PDF preview",
-                interactive=False,
-                visible=True,
-                height=800,
+        with gr.Column(variant="panel", scale=4):
+            pdf_preview = gr.HTML(
+                f"""
+                <div class="pdf-wrapper">
+                <pdfjs-viewer-element
+                    id="pdf-viewer"
+                    viewer-path="gradio_api/file={PDF_JS_DIR}"
+                    locale="en"
+                    style="width: 100%; height: 800px;"
+                >
+                </pdfjs-viewer-element>
+                </div>
+                """
             )
 
         with gr.Column(variant="panel", scale=5):
@@ -299,8 +490,6 @@ with gr.Blocks(
                     output_tabs.append(output_tab)
                     visualization_sub_tabs.append(visual_sub_tab)
 
-    input_file.change(fn=lambda x: x, inputs=input_file, outputs=pdf_preview)
-
     click_event = convert_btn.click(
         fn=show_tabs,
         inputs=[methods],
@@ -328,13 +517,21 @@ with gr.Blocks(
             return msg
 
         def process_method(
-            input_file, selected_methods, use_full_page, force_ocr, method=method
+            input_file,
+            selected_methods,
+            use_full_page,
+            force_ocr,
+            generate_toc,
+            add_reference_links,
+            method=method,
         ):
             return convert_document(
                 input_file,
                 method=method,
                 use_full_page=use_full_page,
                 force_ocr=force_ocr,
+                generate_toc=generate_toc,
+                add_reference_links=add_reference_links,
                 enabled=method in selected_methods,
             )
 
@@ -343,10 +540,23 @@ with gr.Blocks(
             inputs=[methods],
             outputs=[progress_status],
         ).then(
-            fn=lambda input_file, methods, use_full_page, force_ocr, method=method: process_method(  # noqa: E501
-                input_file, methods, use_full_page, force_ocr, method
+            fn=lambda input_file, methods, use_full_page, force_ocr, generate_toc, add_reference_links, method=method: process_method(  # noqa: E501
+                input_file,
+                methods,
+                use_full_page,
+                force_ocr,
+                generate_toc,
+                add_reference_links,
+                method,
             ),
-            inputs=[input_file, methods, full_page_render, force_page_ocr],
+            inputs=[
+                input_file,
+                methods,
+                full_page_render,
+                force_page_ocr,
+                generate_toc,
+                add_reference_links,
+            ],
             outputs=output_components[idx * 4 : (idx + 1) * 4],
         )
 
@@ -361,12 +571,16 @@ with gr.Blocks(
         ]
         + output_components,
         outputs=[output_file],
+    ).then(
+        fn=None,
+        inputs=None,
+        outputs=None,
+        js=ASSIGN_CLICK_JS,
     )
 
     clear_btn.add(
         [
             input_file,
-            pdf_preview,
             output_file,
         ]
         + output_components
@@ -374,6 +588,18 @@ with gr.Blocks(
     clear_btn.click(
         fn=lambda: gr.update(visible=False),
         outputs=[output_file],
+    ).then(
+        fn=None,
+        inputs=None,
+        outputs=output_components,
+        js=CLEAR_PDF_JS,
+    )
+
+    input_file.change(
+        fn=None,
+        inputs=input_file,
+        outputs=None,
+        js=LOAD_PDF_JS,
     )
 
     demo.queue(default_concurrency_limit=4).launch(
@@ -381,5 +607,6 @@ with gr.Blocks(
         max_file_size="50mb",
         allowed_paths=[
             TMP_DIR,
+            PDF_JS_DIR,
         ],
     )
